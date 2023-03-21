fdbbc33fad06b9bb728b28b20e15c0b02350774e
csthomas <cthomas@@newrelic.com>
2023-03-20 22:27:26 -0700

    [NR-96048] Add VariantAdapter and class transform classes
    Refactor variants to use androidComponents as well as the legacy
    android extension, for Gradle installations 7.2 and higher.
    
    Refactor variant code in the plugin into an adapter to manage the old and
    new plugin variant API's. The adapter provides agnostic methods for mutating
    the DAG with tasks we need to instrument, configure and process obfuscation maps.

---
+++ plugins/gradle/src/main/groovy/com/newrelic/agent/android/instrumentation/TraceClassVisitorFactory.groovy
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022-2023. New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.newrelic.agent.android.instrumentation
+
+import com.android.build.api.instrumentation.AsmClassVisitorFactory
+import com.android.build.api.instrumentation.ClassContext
+import com.android.build.api.instrumentation.ClassData
+import com.android.build.api.instrumentation.InstrumentationParameters
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.util.TraceClassVisitor
+
+abstract class TraceClassVisitorFactory implements AsmClassVisitorFactory<VisitorFactoryParams> {
+
+    interface VisitorFactoryParams extends InstrumentationParameters {
+        @Input
+        @Optional
+        abstract Property<Boolean> getTruth()
+    }
+
+    @Override
+    ClassVisitor createClassVisitor(ClassContext classContext, ClassVisitor nextClassVisitor) {
+        def className = classContext.currentClassData.className
+        def classReader = nextClassVisitor.findClassWriter().findClassReader()
+        def isMinifiedClass = classReader.isMinifiedClass() ?: false
+
+        return new TraceClassVisitor(nextClassVisitor, new PrintWriter(new File("trace.asm")))
+    }
+
+    @Override
+    boolean isInstrumentable( ClassData classData) {
+        return classData.className.startsWith("com.newrelic")
+    }
+}
