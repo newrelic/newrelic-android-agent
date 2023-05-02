70b31f68142f9270d61569776d35473c10b93272
ywang <ywang@newrelic.com>
2023-05-02 11:53:00 -0400

    Log forwarding POC

--- agent-core/src/main/java/com/newrelic/agent/android/logging/AgentLogManager.java
+++ agent-core/src/main/java/com/newrelic/agent/android/logging/AgentLogManager.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.newrelic.agent.android.AgentConfiguration;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -33,9 +34,13 @@ public class AgentLogManager {
 
     public static void uploadLog() {
         try {
+            AgentConfiguration agentConfiguration = new AgentConfiguration();
+
             JsonArray jsonArr = new JsonArray();
             JsonObject jsonObj = new JsonObject();
             jsonObj.addProperty("message", "test from android agent");
+            jsonObj.addProperty("entity.guid", "MjUxOTY2NnxNT0JJTEV8QVBQTElDQVRJT058NTM0NTQ0NTUz");
+            jsonObj.addProperty("sessionId", agentConfiguration.getSessionID());
             jsonArr.add(jsonObj);
 
             URL url = new URL("https://log-api.newrelic.com/log/v1?Api-Key=f57026e8e5904c4a81dd9a267ae30971FFFFNRAL");
