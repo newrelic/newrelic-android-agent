af27596e38f36f2cdc5a0147478d523dfd1662df
Cameron Thomas <cthomas@newrelic.com>
2024-07-19 10:21:33 -0700

    [NR-233276] chore: Refactor AgentNDKListener
    * Fix one *potential* leak in harvest connection output buffer.
    * Adds and refactors test resources

--- agent/src/main/java/com/newrelic/agent/android/ndk/NativeReporting.java
+++ agent/src/main/java/com/newrelic/agent/android/ndk/NativeReporting.java
@@ -9,9 +9,9 @@ import android.content.Context;
 
 import com.newrelic.agent.android.Agent;
 import com.newrelic.agent.android.AgentConfiguration;
-import com.newrelic.agent.android.FeatureFlag;
 import com.newrelic.agent.android.NewRelic;
 import com.newrelic.agent.android.agentdata.AgentDataController;
+import com.newrelic.agent.android.agentdata.AgentDataReporter;
 import com.newrelic.agent.android.analytics.AnalyticsAttribute;
 import com.newrelic.agent.android.analytics.AnalyticsControllerImpl;
 import com.newrelic.agent.android.crash.CrashReporter;
@@ -28,18 +28,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class NativeReporting extends HarvestAdapter implements AgentNDKListener {
+public class NativeReporting extends HarvestAdapter {
     protected static final AgentLog log = AgentLogManager.getAgentLog();
     protected static AtomicReference<NativeReporting> instance = new AtomicReference<>(null);
+    protected static AtomicReference<AgentNDK> agentNdk = new AtomicReference<AgentNDK>(null);
+
+    protected final NativeReportListener nativeReportListener;
 
     public static NativeReporting getInstance() {
         return instance.get();
     }
 
+
     public static NativeReporting initialize(Context context, AgentConfiguration agentConfiguration) {
         instance.compareAndSet(null, new NativeReporting(context, agentConfiguration));
         Harvest.addHarvestListener(instance.get());
-        StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_INIT);
+        StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_INIT);
         return instance.get();
     }
 
@@ -52,41 +56,47 @@ public class NativeReporting extends HarvestAdapter implements AgentNDKListener 
     }
 
     public static boolean isInitialized() {
-        return (instance.get() != null) && (AgentNDK.getInstance() != null);
+        return (instance.get() != null && agentNdk.get() != null) && (AgentNDK.getInstance() != null);
     }
 
     public static void crashNow(final String message) {
         if (isInitialized()) {
-            AgentNDK.getInstance().crashNow(message);
+            agentNdk.get().crashNow(message);
         }
     }
 
     public static boolean isRooted() {
         boolean isRooted = false;
         if (isInitialized()) {
-            StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_ROOTED_DEVICE);
+            StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_ROOTED_DEVICE);
             isRooted = AgentNDK.getInstance().isRooted();
         }
         return isRooted;
     }
 
-    NativeReporting(Context context, AgentConfiguration agentConfiguration) {
-        AgentNDK.Builder builder = new AgentNDK.Builder(context);
-        builder.withBuildId(Agent.getBuildId())
-                .withANRMonitor(!agentConfiguration.getApplicationExitConfiguration().isEnabled())
-                .withSessionId(agentConfiguration.getSessionID())
-                .withReportListener(this)
-                .withLogger(log)
-                .build();
+    protected NativeReporting(Context context, AgentConfiguration agentConfiguration) {
+        this.nativeReportListener = new NativeReportListener();
+        this.agentNdk.compareAndSet(null,
+                new AgentNDK.Builder(context)
+                        .withBuildId(Agent.getBuildId())
+                        .withANRMonitor(!agentConfiguration.getApplicationExitConfiguration().isEnabled())
+                        .withSessionId(agentConfiguration.getSessionID())
+                        .withReportListener(this.nativeReportListener)
+                        .withLogger(AgentLogManager.getAgentLog())
+                        .build());
     }
 
     public void start() {
         if (NativeReporting.isInitialized()) {
-            AgentNDK.getInstance().start();
-            StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_START);
+            try {
+                agentNdk.get().start();
+                StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_START);
+            } catch (Exception e) {
+                log.error(e.toString());
+            }
             boolean isRooted = isRooted();
             if (isRooted)
-                NewRelic.setAttribute("RootedDevice", isRooted);
+                NewRelic.setAttribute(AnalyticsAttribute.NATIVE_ROOTED_DEVICE_ATTRIBUTE, isRooted);
         } else {
             log.error("CrashReporter: Must first initialize native module.");
         }
@@ -94,74 +104,20 @@ public class NativeReporting extends HarvestAdapter implements AgentNDKListener 
 
     void stop() {
         if (NativeReporting.isInitialized()) {
-            AgentNDK.getInstance().stop();
-            StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_STOP);
+            try {
+                agentNdk.get().stop();
+            } catch (Exception e) {
+                log.error(e.toString());
+            }
+            StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_STOP);
+            agentNdk.set(null);
         }
     }
 
     @Override
-    public boolean onNativeCrash(String crashAsString) {
-        StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_CRASH);
-
-        final AnalyticsControllerImpl analyticsController = AnalyticsControllerImpl.getInstance();
-        final Set<AnalyticsAttribute> sessionAttributes = new HashSet<AnalyticsAttribute>() {{
-            addAll(analyticsController.getSessionAttributes());
-        }};
-
-        NativeException exceptionToHandle = new NativeCrashException(crashAsString);
-
-        sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native"));
-        sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.NATIVE_CRASH, true));
-        sessionAttributes.add(new AnalyticsAttribute("exceptionMessage", exceptionToHandle.getNativeStackTrace().getExceptionMessage()));
-        sessionAttributes.add(new AnalyticsAttribute("crashingThreadId", exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId()));
-
-        NativeCrash crash = new NativeCrash(exceptionToHandle,
-                sessionAttributes,
-                analyticsController.getEventManager().getQueuedEvents());
-
-        CrashReporter.getInstance().storeAndReportCrash(crash);
-
-        return true;
-    }
-
-    @Override
-    public boolean onNativeException(String exceptionAsString) {
-        StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_EXCEPTION);
-
-        Map<String, Object> exceptionAttributes = new HashMap<String, Object>() {{
-            put(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native");
-            put(AnalyticsAttribute.UNHANDLED_NATIVE_EXCEPTION, true);
-        }};
-
-        NativeException exceptionToHandle = new NativeUnhandledException(exceptionAsString);
-        exceptionAttributes.put("crashingThreadId", exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId());
-        exceptionAttributes.put("nativeThreads", exceptionToHandle.getNativeStackTrace().getThreads());
-        exceptionAttributes.put("exceptionMessage", exceptionToHandle.getNativeStackTrace().getExceptionMessage());
-
-        return AgentDataController.sendAgentData(exceptionToHandle, exceptionAttributes);
-    }
-
-    @Override
-    public boolean onApplicationNotResponding(String anrAsString) {
-        StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_ANR);
-
-        Map<String, Object> exceptionAttributes = new HashMap<String, Object>() {{
-            put(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native");
-            put(AnalyticsAttribute.ANR, true);
-        }};
-
-        NativeException exceptionToHandle = new ANRException(anrAsString);
-        exceptionAttributes.put("crashingThreadId", exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId());
-        exceptionAttributes.put("nativeThreads", exceptionToHandle.getNativeStackTrace().getThreads());
-        exceptionAttributes.put("exceptionMessage", exceptionToHandle.getNativeStackTrace().getExceptionMessage());
-
-        return AgentDataController.sendAgentData(exceptionToHandle, exceptionAttributes);
-    }
-
-    @Override
     public void onHarvestStart() {
-        AgentNDK.getInstance().flushPendingReports();
-        StatsEngine.get().inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_FLUSH);
+        agentNdk.get().flushPendingReports();
+        StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_FLUSH);
     }
 
 
@@ -183,4 +139,91 @@ public class NativeReporting extends HarvestAdapter implements AgentNDKListener 
         }
     }
 
+    static class NativeReportListener implements AgentNDKListener {
+        @Override
+        public boolean onNativeCrash(String crashAsString) {
+            StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_CRASH);
+
+            final AnalyticsControllerImpl analyticsController = AnalyticsControllerImpl.getInstance();
+            final Set<AnalyticsAttribute> sessionAttributes = new HashSet<AnalyticsAttribute>() {{
+                addAll(analyticsController.getSessionAttributes());
+            }};
+
+            NativeException exceptionToHandle = new NativeCrashException(crashAsString);
+
+            sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native"));
+            sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.NATIVE_CRASH, true));
+            sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.NATIVE_EXCEPTION_MESSAGE_ATTRIBUTE, exceptionToHandle.getNativeStackTrace().getExceptionMessage()));
+
+            if (null != exceptionToHandle.getNativeStackTrace().getCrashedThread()) {
+                sessionAttributes.add(new AnalyticsAttribute(AnalyticsAttribute.NATIVE_CRASHING_THREAD_ID_ATTRIBUTE, exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId()));
+            }
+
+            NativeCrash crash = new NativeCrash(exceptionToHandle,
+                    sessionAttributes,
+                    analyticsController.getEventManager().getQueuedEvents());
+
+            if (null != CrashReporter.getInstance()) {
+                CrashReporter.getInstance().storeAndReportCrash(crash);
+                return true;
+            } else {
+                log.error("Could not report native crash: CrashReporter is disabled.");
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onNativeException(String exceptionAsString) {
+            StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_EXCEPTION);
+
+            Map<String, Object> exceptionAttributes = new HashMap<String, Object>() {{
+                put(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native");
+                put(AnalyticsAttribute.UNHANDLED_NATIVE_EXCEPTION, true);
+            }};
+
+            NativeException exceptionToHandle = new NativeUnhandledException(exceptionAsString);
+            exceptionAttributes.put(AnalyticsAttribute.NATIVE_THREADS_ATTRIBUTE, exceptionToHandle.getNativeStackTrace().getThreads());
+            exceptionAttributes.put(AnalyticsAttribute.NATIVE_EXCEPTION_MESSAGE_ATTRIBUTE, exceptionToHandle.getNativeStackTrace().getExceptionMessage());
+
+            if (null != exceptionToHandle.getNativeStackTrace().getCrashedThread()) {
+                exceptionAttributes.put(AnalyticsAttribute.NATIVE_CRASHING_THREAD_ID_ATTRIBUTE, exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId());
+            }
+
+            if (null != AgentDataReporter.getInstance()) {
+                return AgentDataController.sendAgentData(exceptionToHandle, exceptionAttributes);
+            } else {
+                log.error("Could not report native exception: AgentDataReporter is disabled.");
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onApplicationNotResponding(String anrAsString) {
+            StatsEngine.SUPPORTABILITY.inc(MetricNames.SUPPORTABILITY_NDK_REPORTS_ANR);
+
+            Map<String, Object> exceptionAttributes = new HashMap<String, Object>() {{
+                put(AnalyticsAttribute.APPLICATION_PLATFORM_ATTRIBUTE, "native");
+                put(AnalyticsAttribute.ANR, true);
+            }};
+
+            NativeException exceptionToHandle = new ANRException(anrAsString);
+            exceptionAttributes.put("nativeThreads", exceptionToHandle.getNativeStackTrace().getThreads());
+            exceptionAttributes.put("exceptionMessage", exceptionToHandle.getNativeStackTrace().getExceptionMessage());
+
+            if (null != exceptionToHandle.getNativeStackTrace().getCrashedThread()) {
+                exceptionAttributes.put("crashingThreadId", exceptionToHandle.getNativeStackTrace().getCrashedThread().getThreadId());
+            }
+
+            if (null != AgentDataReporter.getInstance()) {
+                return AgentDataController.sendAgentData(exceptionToHandle, exceptionAttributes);
+            } else {
+                log.error("Could not report native exception: AgentDataReporter is disabled.");
+            }
+
+            return false;
+        }
+
+    }
 }
--- agent/src/test/java/com/newrelic/agent/android/ndk/NativeReportingTest.java
+++ agent/src/test/java/com/newrelic/agent/android/ndk/NativeReportingTest.java
@@ -5,43 +5,157 @@
 
 package com.newrelic.agent.android.ndk;
 
+import androidx.annotation.Nullable;
+
 import com.newrelic.agent.android.AgentConfiguration;
+import com.newrelic.agent.android.FeatureFlag;
+import com.newrelic.agent.android.NewRelic;
 import com.newrelic.agent.android.SpyContext;
+import com.newrelic.agent.android.logging.AgentLog;
+import com.newrelic.agent.android.logging.AgentLogManager;
+import com.newrelic.agent.android.logging.ConsoleAgentLog;
+import com.newrelic.agent.android.metric.MetricNames;
+import com.newrelic.agent.android.stats.StatsEngine;
+import com.newrelic.agent.android.util.Streams;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
-
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 @RunWith(RobolectricTestRunner.class)
 public class NativeReportingTest {
 
-    private SpyContext context;
+    private static File reportsDir;
+
     private AgentConfiguration agentConfig;
-    private NativeReporting module;
+    private NativeReporting nativeReporter;
+    private AgentNDK agentNDK;
+    private AgentNDKListener agentNDKListener;
+
+    HashMap<String, AtomicInteger> counters = new HashMap<String, AtomicInteger>() {{
+        put("crash", new AtomicInteger(0));
+        put("exception", new AtomicInteger(0));
+        put("anr", new AtomicInteger(0));
+    }};
+
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        reportsDir = Files.createTempDirectory("com.newrelic.nativeReporting.").toFile();
+        reportsDir.mkdirs();
+
+        FeatureFlag.disableFeature(FeatureFlag.HandledExceptions);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        Assert.assertTrue(reportsDir.exists() && reportsDir.isDirectory());
+        Streams.list(reportsDir).forEach(file -> file.delete());
+        Assert.assertTrue(reportsDir.delete());
+    }
 
     @Before
     public void setup() {
-        context = new SpyContext();
-        agentConfig = new AgentConfiguration();
-        module = NativeReporting.initialize(context.getContext(), agentConfig);
+
+        AgentLogManager.setAgentLog(new ConsoleAgentLog());
+        AgentLogManager.getAgentLog().setLevel(AgentLog.DEBUG);
+
+        NewRelic.enableFeature(FeatureFlag.NativeReporting);
+
+        SpyContext context = new SpyContext();
+
+        agentNDKListener = Mockito.spy(new AgentNDKListener() {
+            @Override
+            public boolean onNativeCrash(@Nullable String s) {
+                counters.get("crash").incrementAndGet();
+                nativeReporter.nativeReportListener.onNativeCrash(s);
+                return true;
+            }
+
+            @Override
+            public boolean onNativeException(@Nullable String s) {
+                counters.get("exception").incrementAndGet();
+                nativeReporter.nativeReportListener.onNativeException(s);
+                return true;
+            }
+
+            @Override
+            public boolean onApplicationNotResponding(@Nullable String s) {
+                counters.get("anr").incrementAndGet();
+                nativeReporter.nativeReportListener.onApplicationNotResponding(s);
+                return true;
+            }
+        });
+
+        StatsEngine.reset();
+
+        agentNDK = Mockito.spy(new AgentNDK.Builder(context.getContext())
+                .withStorageDir(reportsDir)
+                .withANRMonitor(true)
+                .withBuildId(UUID.randomUUID().toString())
+                .withReportListener(agentNDKListener)
+                .withLogger(AgentLogManager.getAgentLog())
+                .withSessionId(UUID.randomUUID().toString())
+                .build());
+
+        agentConfig = AgentConfiguration.getInstance();
+        nativeReporter = Mockito.spy(NativeReporting.initialize(context.getContext(), agentConfig));
+
+        NativeReporting.instance.set(nativeReporter);
+    }
+
+    @Before
+    public void seedReportData() throws IOException {
+        File reportsDir = agentNDK.getManagedContext().getReportsDir();
+
+        Streams.newBufferedFileWriter(new File(reportsDir, "crash-" + UUID.randomUUID().toString())).append("{'crash':{}}").flush();
+        Streams.newBufferedFileWriter(new File(reportsDir, "ex-" + UUID.randomUUID().toString())).append("{'exception':{}}").flush();
+        Streams.newBufferedFileWriter(new File(reportsDir, "anr-" + UUID.randomUUID().toString())).append("{'anr':{}}").flush();
+        Streams.newBufferedFileWriter(new File(reportsDir, "ex-" + UUID.randomUUID().toString())).append("{'exception':{}}").flush();
+        Streams.newBufferedFileWriter(new File(reportsDir, "anr-" + UUID.randomUUID().toString())).append("{'anr':{}}").flush();
+        Streams.newBufferedFileWriter(new File(reportsDir, "crash-" + UUID.randomUUID().toString())).append("{'crash':{}}").flush();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Streams.list(agentNDK.getManagedContext().getReportsDir()).forEach(file -> Assert.assertTrue(file.delete()));
+        Assert.assertTrue(agentNDK.getManagedContext().getReportsDir().delete());
+        Assert.assertTrue(agentNDK.getManagedContext().getReportsDir().getParentFile().delete());
+        NativeReporting.instance.set(null);
     }
 
     @Test
     public void getInstance() {
-        Assert.assertEquals(module, NativeReporting.getInstance());
+        Assert.assertEquals(nativeReporter, NativeReporting.getInstance());
     }
 
     @Test
     public void initialize() {
-        NativeReporting module = NativeReporting.initialize(context.getContext(), agentConfig);
         Assert.assertTrue(NativeReporting.isInitialized());
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_INIT));
     }
 
     @Test
+    @Ignore("FIXME: java.lang.UnsatisfiedLinkError")
     public void shutdown() {
+        NativeReporting.shutdown();
+        Assert.assertFalse(NativeReporting.isInitialized());
+        Assert.assertNull(NativeReporting.getInstance());
     }
 
     @Test
@@ -50,26 +164,101 @@ public class NativeReportingTest {
     }
 
     @Test
-    public void start() {
+    public void isRooted() {
+        NativeReporting.isRooted();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_ROOTED_DEVICE));
     }
 
     @Test
+    @Ignore("FIXME: java.lang.UnsatisfiedLinkError")
+    public void start() {
+        nativeReporter.start();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_START));
+    }
+
+    @Test
+    @Ignore("FIXME: java.lang.UnsatisfiedLinkError")
     public void stop() {
+        nativeReporter.stop();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_STOP));
     }
 
     @Test
     public void onNativeCrash() {
+        agentNDK.flushPendingReports();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_REPORTS_CRASH));
+        Assert.assertEquals("anr", 2, counters.get("crash").get());
     }
 
     @Test
     public void onNativeException() {
+        agentNDK.flushPendingReports();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_REPORTS_EXCEPTION));
+        Assert.assertEquals("anr", 2, counters.get("exception").get());
     }
 
     @Test
     public void onApplicationNotResponding() {
+        agentNDK.flushPendingReports();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_REPORTS_ANR));
+        Assert.assertEquals("anr", 2, counters.get("anr").get());
     }
 
     @Test
     public void onHarvestStart() {
+        nativeReporter.onHarvestStart();
+        Assert.assertTrue(statsEngineContains(MetricNames.SUPPORTABILITY_NDK_REPORTS_FLUSH));
+        counters.entrySet().forEach(c -> Assert.assertEquals(c.getKey(), 0, c.getValue().get()));
     }
+
+    @Test
+    public void flushPendingReports() {
+        Assert.assertEquals(6, Streams.list(agentNDK.getManagedContext().getReportsDir()).count());
+        agentNDK.flushPendingReports();
+        counters.entrySet().forEach(c -> Assert.assertEquals(c.getKey(), 2, c.getValue().get()));
+        Assert.assertEquals(0, Streams.list(agentNDK.getManagedContext().getReportsDir()).count());
+    }
+
+    @Test
+    public void testNativeException() throws IOException {
+        String json = Streams.slurpString(NativeReporting.class.getResourceAsStream("/nativeReporting/crash-2024.sample"));
+        NativeException e = new NativeException(json);
+        Assert.assertNotNull(e.getStackTrace());
+        Assert.assertNotNull(e.getNativeStackTrace());
+        Assert.assertFalse(e.getNativeStackTrace().getExceptionMessage().isEmpty());
+    }
+
+    @Test
+    public void testNativeCrashException() throws IOException {
+        String json = Streams.slurpString(NativeReporting.class.getResourceAsStream("/nativeReporting/crash-2024.sample"));
+        NativeReporting.NativeCrashException e = new NativeReporting.NativeCrashException(json);
+        Assert.assertFalse(e.getNativeStackTrace().getExceptionMessage().isEmpty());
+        Assert.assertFalse(e.getNativeStackTrace().getThreads().isEmpty());
+        Assert.assertNotNull(e.getNativeStackTrace().getCrashedThread());
+    }
+
+    @Test
+    public void testNativeUnhandledException() throws IOException {
+        String json = Streams.slurpString(NativeReporting.class.getResourceAsStream("/nativeReporting/exception-2024.sample"));
+        NativeReporting.NativeUnhandledException e = new NativeReporting.NativeUnhandledException(json);
+        Assert.assertNotNull(e.getNativeStackTrace());
+        Assert.assertFalse(e.getNativeStackTrace().getExceptionMessage().isEmpty());
+        Assert.assertFalse(e.getNativeStackTrace().getThreads().isEmpty());
+        Assert.assertNotNull(e.getNativeStackTrace().getCrashedThread());
+    }
+
+    @Test
+    public void testANRException() throws IOException {
+        String json = Streams.slurpString(NativeReporting.class.getResourceAsStream("/nativeReporting/anr-2024.sample"));
+        NativeReporting.ANRException e = new NativeReporting.ANRException(json);
+        Assert.assertNotNull(e.getNativeStackTrace());
+        Assert.assertFalse(e.getNativeStackTrace().getExceptionMessage().isEmpty());
+        Assert.assertFalse(e.getNativeStackTrace().getThreads().isEmpty());
+        Assert.assertNotNull(e.getNativeStackTrace().getCrashedThread());
+    }
+
+    private boolean statsEngineContains(String metricName) {
+        return StatsEngine.SUPPORTABILITY.getStatsMap().keySet().contains(metricName);
+    }
+
 }
