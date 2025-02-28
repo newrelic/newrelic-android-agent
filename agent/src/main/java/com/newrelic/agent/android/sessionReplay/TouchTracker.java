c349d74d0da54fc9c6354b67397c3d245098942f
Steve Malsam <smalsam@newrelic.com>
2025-02-28 14:48:54 -0800

    Added Touch support

--- agent/src/main/java/com/newrelic/agent/android/sessionReplay/TouchTracker.java
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/TouchTracker.java
@@ -1,53 +1,23 @@
 package com.newrelic.agent.android.sessionReplay;
 
-import com.newrelic.agent.android.sessionReplay.models.RRWebRRWebTouchUpDownData;
-import com.newrelic.agent.android.sessionReplay.models.RRWebTouch;
-import com.newrelic.agent.android.sessionReplay.models.RRWebTouchData;
-import com.newrelic.agent.android.sessionReplay.models.RRWebTouchMoveData;
-import com.newrelic.agent.android.sessionReplay.models.RecordedTouchData;
+import com.newrelic.agent.android.sessionReplay.models.Touch;
 
 import java.util.ArrayList;
 
 public class TouchTracker {
-    private RecordedTouchData startTouch;
-    private RecordedTouchData endTouch;
-    private ArrayList<RecordedTouchData> moveTouches = new ArrayList<>();
+    private Touch startTouch;
+    private Touch endTouch;
+    private ArrayList<Touch> moveTouches;
 
-    public TouchTracker(RecordedTouchData startTouch) {
+    public TouchTracker(Touch startTouch) {
         this.startTouch = startTouch;
     }
 
-    public void addMoveTouch(RecordedTouchData touch) {
+    public void addMoveTouch(Touch touch) {
         moveTouches.add(touch);
     }
 
-    public void addEndTouch(RecordedTouchData touch) {
+    public void addEndTouch(Touch touch) {
         endTouch = touch;
     }
-
-    public ArrayList<RRWebTouch> processTouchData() {
-        ArrayList<RRWebTouch> touches = new ArrayList<>();
-
-        RRWebRRWebTouchUpDownData downTouch = new RRWebRRWebTouchUpDownData(2, 7, startTouch.originatingViewId, startTouch.xCoordinate, startTouch.yCoordinate);
-        touches.add(new RRWebTouch(startTouch.timestamp, 3, downTouch));
-
-        if(!moveTouches.isEmpty()) {
-            RecordedTouchData lastTouch = moveTouches.get(moveTouches.size()-1);
-            long lastTimestamp = lastTouch.timestamp;
-            ArrayList<RRWebTouchMoveData.Position> movePositions = new ArrayList<>();
-            for(RecordedTouchData moveTouch : moveTouches) {
-                RRWebTouchMoveData.Position position = new RRWebTouchMoveData.Position(moveTouch.originatingViewId, moveTouch.xCoordinate, moveTouch.yCoordinate, (moveTouch.timestamp - lastTimestamp));
-                movePositions.add(position);
-            }
-
-            RRWebTouchMoveData moveData = new RRWebTouchMoveData(1, movePositions);
-            touches.add(new RRWebTouch(lastTimestamp, 3, moveData));
-        }
-
-        RRWebRRWebTouchUpDownData upTouch = new RRWebRRWebTouchUpDownData(2, 9, endTouch.originatingViewId, endTouch.xCoordinate, endTouch.yCoordinate);
-        touches.add(new RRWebTouch(endTouch.timestamp, 3, upTouch));
-
-        return touches;
-    }
-
 }
