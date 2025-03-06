60d6e8de10ad5945e34af044d06f8521cbf01e04
Steve Malsam <smalsam@newrelic.com>
2025-03-06 09:34:51 -0800

    Set up ground work for system

--- agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayActivityLifecycleCallbacks.java
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayActivityLifecycleCallbacks.java
@@ -54,6 +54,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 
 public class SessionReplayActivityLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
