6f561a9f8d7c9ec953dda4aa71c738266168f6d5
Steve Malsam <smalsam@newrelic.com>
2025-03-06 09:34:51 -0800

    Set up ground work for system

---
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/ViewDrawInterceptor.java
@@ -0,0 +1,59 @@
+package com.newrelic.agent.android.sessionReplay;
+
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class ViewDrawInterceptor {
+    private WeakHashMap<View, ViewTreeObserver.OnDrawListener> decorViewListeners = new WeakHashMap<>();
+
+    public void Intercept(View[] decorViews) {
+        stopInterceptAndRemove(decorViews);
+        ViewTreeObserver.OnDrawListener listener = new ViewTreeObserver.OnDrawListener() {
+            @Override
+            public void onDraw() {
+                // Start walking the view tree
+            }
+
+        };
+
+        for(View decorView : decorViews) {
+            ViewTreeObserver viewTreeObserver = decorView.getViewTreeObserver();
+            if(viewTreeObserver != null && viewTreeObserver.isAlive()) {
+                try{
+                    viewTreeObserver.addOnDrawListener(listener);
+                    decorViewListeners.put(decorView, listener);
+                } catch (IllegalStateException e) {
+                    Log.e("ViewDrawInterceptor", "Unable to add onDrawListener!");
+                }
+            }
+        }
+    }
+
+    public void stopIntercept() {
+        for(Map.Entry<View, ViewTreeObserver.OnDrawListener> entry : decorViewListeners.entrySet()) {
+            safeObserverRemoval(entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void stopInterceptAndRemove(View[] decorViews) {
+        for(View view : decorViews) {
+            ViewTreeObserver.OnDrawListener listener = decorViewListeners.get(view);
+            safeObserverRemoval(view, listener);
+        }
+        decorViewListeners.clear();
+    }
+
+    private void safeObserverRemoval(View view, ViewTreeObserver.OnDrawListener listener) {
+        if(view.getViewTreeObserver().isAlive()) {
+            try {
+                view.getViewTreeObserver().removeOnDrawListener(listener);
+            } catch(IllegalStateException e) {
+                Log.e("ViewDrawInterceptor", "Unable to remove onDrawListener!");
+            }
+        }
+    }
+}
