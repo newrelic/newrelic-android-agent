814d69ce9c998af15be49a5eb60c1a3d4a56d3d3
Steve Malsam <smalsam@newrelic.com>
2025-03-17 16:42:33 -0700

    Added RRWeb models; Created Processor class
    The SessionReplayProcessor does the work of converting the SessionReplayThingies into RRWeb models. These models will then be used by GSON to convert into the JSON fed to the uploader.

---
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/models/RRWebMetaEvent.java
@@ -0,0 +1,24 @@
+package com.newrelic.agent.android.sessionReplay.models;
+
+public class RRWebMetaEvent implements RRWebEvent {
+    public static class RRWebMetaEventData {
+        public String href;
+        public int width;
+        public int height;
+
+        public RRWebMetaEventData(String href, int width, int height) {
+            this.href = href;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    public int type = RRWE_EVENT_META;
+    public RRWebMetaEventData data;
+    public long timestamp;
+
+    public RRWebMetaEvent(RRWebMetaEventData data, long timestamp) {
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
