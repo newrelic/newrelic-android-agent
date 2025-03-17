5bf4e0258345db89eccff6c6a7e8ae268795f7d7
Steve Malsam <smalsam@newrelic.com>
2025-03-17 16:42:33 -0700

    Added RRWeb models; Created Processor class
    The SessionReplayProcessor does the work of converting the SessionReplayThingies into RRWeb models. These models will then be used by GSON to convert into the JSON fed to the uploader.

--- agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayFrame.java
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayFrame.java
@@ -1,8 +1,8 @@
 package com.newrelic.agent.android.sessionReplay;
 
 public class SessionReplayFrame {
-    private long timestamp;
-    private SessionReplayThingy rootThingy;
+    public long timestamp;
+    public SessionReplayThingy rootThingy;
 
 
     public SessionReplayFrame(SessionReplayThingy rootThingy, long timestamp) {
--- agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayThingy.java
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayThingy.java
@@ -1,6 +1,5 @@
 package com.newrelic.agent.android.sessionReplay;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SessionReplayThingy {
@@ -17,13 +16,51 @@ public class SessionReplayThingy {
     public Rect position;
     public int id;
     public String name;
-    public List<SessionReplayThingy> childNodes = new ArrayList<>();
+    public List<SessionReplayThingy> childNodes;
 
     public SessionReplayThingy(Rect position,
                                String name,
-                               int id) {
+                               int id,
+                               List<SessionReplayThingy> childNodes) {
         this.position = position;
         this.name = name;
         this.id = id;
+        this.childNodes = childNodes;
+    }
+
+    public String generateCssDescription() {
+        StringBuilder cssString = new StringBuilder();
+        String cssSelector = generateCssSelector();
+
+        cssString.append("#")
+                .append(cssSelector)
+                .append("{")
+                .append(generatePositionCss());
+
+        return cssString.toString();
+    }
+
+    public String generateCssSelector() {
+        return (name + "-" + id);
+    }
+
+    private StringBuilder generatePositionCss() {
+        StringBuilder positionStringBuilder = new StringBuilder();
+
+        positionStringBuilder.append("position: fixed;")
+                .append("left: ")
+                .append(position.x)
+                .append("px;")
+                .append("top: ")
+                .append(position.y)
+                .append("px;")
+                .append("width: ")
+                .append(position.width)
+                .append("px;")
+                .append("height: ")
+                .append(position.height)
+                .append("px;");
+
+        return positionStringBuilder;
     }
 }
