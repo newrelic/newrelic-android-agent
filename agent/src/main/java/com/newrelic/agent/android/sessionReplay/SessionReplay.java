58ba348b32c06f5c8abad723a811174503ae75d6
Steve Malsam <smalsam@newrelic.com>
2025-03-17 16:42:33 -0700

    Added RRWeb models; Created Processor class
    The SessionReplayProcessor does the work of converting the SessionReplayThingies into RRWeb models. These models will then be used by GSON to convert into the JSON fed to the uploader.

--- agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplay.java
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplay.java
@@ -1,14 +1,22 @@
 package com.newrelic.agent.android.sessionReplay;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 
+import com.google.gson.Gson;
 import com.newrelic.agent.android.sessionReplay.internal.Curtains;
 import com.newrelic.agent.android.sessionReplay.internal.OnFrameTakenListener;
 import com.newrelic.agent.android.sessionReplay.internal.RootViewsSpy;
+import com.newrelic.agent.android.sessionReplay.models.RRWebEvent;
+import com.newrelic.agent.android.sessionReplay.models.RRWebMetaEvent;
 
 import java.util.ArrayList;
 
@@ -16,6 +24,7 @@ public class SessionReplay implements OnFrameTakenListener {
     private Application application;
     private Handler uiThreadHandler;
     private SessionReplayActivityLifecycleCallbacks sessionReplayActivityLifecycleCallbacks;
+    private SessionReplayProcessor processor = new SessionReplayProcessor();
     private ViewDrawInterceptor viewDrawInterceptor;
 
     private ArrayList<SessionReplayFrame> rawFrames = new ArrayList<>();
@@ -67,5 +76,20 @@ public class SessionReplay implements OnFrameTakenListener {
     @Override
     public void onFrameTaken(@NonNull SessionReplayFrame newFrame) {
         rawFrames.add(newFrame);
+
+        if(rawFrames.size() > 5) {
+            WindowManager wm = (WindowManager) application.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+
+            RRWebMetaEvent metaEvent = new RRWebMetaEvent(new RRWebMetaEvent.RRWebMetaEventData("https://newrelic.com", size.x, size.y), System.currentTimeMillis());
+            ArrayList<RRWebEvent> rrWebEvents = new ArrayList<>();
+            rrWebEvents.add(metaEvent);
+
+            rrWebEvents.addAll(processor.processFrames(rawFrames));
+            String json = new Gson().toJson(rrWebEvents);
+            Log.d("Session Replay", "jsonPayload in RRWeb: " + json);
+        }
     }
 }
