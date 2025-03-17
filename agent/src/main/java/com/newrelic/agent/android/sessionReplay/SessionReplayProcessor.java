6042dedc2ea33301dc02b1793f8ef6fa3426b647
Steve Malsam <smalsam@newrelic.com>
2025-03-17 16:42:33 -0700

    Added RRWeb models; Created Processor class
    The SessionReplayProcessor does the work of converting the SessionReplayThingies into RRWeb models. These models will then be used by GSON to convert into the JSON fed to the uploader.

---
+++ agent/src/main/java/com/newrelic/agent/android/sessionReplay/SessionReplayProcessor.java
@@ -0,0 +1,70 @@
+package com.newrelic.agent.android.sessionReplay;
+
+import com.newrelic.agent.android.sessionReplay.models.Attributes;
+import com.newrelic.agent.android.sessionReplay.models.InitialOffset;
+import com.newrelic.agent.android.sessionReplay.models.RRWebElementNode;
+import com.newrelic.agent.android.sessionReplay.models.RRWebEvent;
+import com.newrelic.agent.android.sessionReplay.models.RRWebFullSnapshotEvent;
+import com.newrelic.agent.android.sessionReplay.models.RRWebNode;
+import com.newrelic.agent.android.sessionReplay.models.RRWebTextNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SessionReplayProcessor {
+    public static int RRWEB_TYPE_FULL_SNAPSHOT = 2;
+    public static int RRWEB_TYPE_INCREMENTAL_SNAPSHOT = 3;
+
+    public List<RRWebEvent> processFrames(List<SessionReplayFrame> rawFrames) {
+        ArrayList<RRWebEvent> snapshot = new ArrayList<>();
+
+        for(SessionReplayFrame rawFrame : rawFrames) {
+            snapshot.add(processFrame(rawFrame));
+        }
+
+        return snapshot;
+    }
+
+    private RRWebFullSnapshotEvent processFrame(SessionReplayFrame frame) {
+        // Generate style string
+        StringBuilder cssStyleBuilder = new StringBuilder();
+
+        // Generate Body node
+        RRWebElementNode rootElement = recursivelyProcessThingy(frame.rootThingy, cssStyleBuilder);
+
+        // Generate boilerplate nodes
+        // CSS node
+        RRWebTextNode cssText = new RRWebTextNode(cssStyleBuilder.toString(), true, NewRelicIdGenerator.generateId());
+        RRWebElementNode cssNode = new RRWebElementNode(null, RRWebElementNode.TAG_TYPE_STYLE, NewRelicIdGenerator.generateId(), Collections.singletonList(cssText));
+
+        // Head Node
+        RRWebElementNode headNode = new RRWebElementNode(null, RRWebElementNode.TAG_TYPE_HEAD, NewRelicIdGenerator.generateId(), Collections.singletonList(cssNode));
+
+        // Body node
+        RRWebElementNode bodyNode = new RRWebElementNode(null, RRWebElementNode.TAG_TYPE_BODY, NewRelicIdGenerator.generateId(), Collections.singletonList(rootElement));
+
+        // HTML node
+        RRWebElementNode htmlNode = new RRWebElementNode(null, RRWebElementNode.TAG_TYPE_HTML,NewRelicIdGenerator.generateId(), Arrays.asList(headNode, bodyNode));
+
+        // Create Full Snapshot Root and add the above to it
+        RRWebFullSnapshotEvent fullSnapshotEvent = new RRWebFullSnapshotEvent(new InitialOffset(0, 0), frame.timestamp, Collections.singletonList(htmlNode));
+
+        return fullSnapshotEvent;
+    }
+
+    private RRWebElementNode recursivelyProcessThingy(SessionReplayThingy rootThingy, StringBuilder cssStyleBuilder) {
+        cssStyleBuilder.append(rootThingy.generateCssDescription());
+
+        Attributes attribues = new Attributes(rootThingy.generateCssSelector());
+        ArrayList<RRWebNode> childNodes = new ArrayList<>();
+        for(SessionReplayThingy childNode : rootThingy.childNodes) {
+            RRWebElementNode childElement = recursivelyProcessThingy(childNode, cssStyleBuilder);
+            childNodes.add(childElement);
+        }
+
+        return new RRWebElementNode(attribues, RRWebElementNode.TAG_TYPE_DIV, rootThingy.id, childNodes);
+    }
+
+}
