87560fb7868265366a09b943a58a6764bfcd8e21
ywang-nr <ywang@newrelic.com>
2026-09-02 13:43:18 -0400

    fix: record MobileView events (missing case in AnalyticsEventFactory)

--- agent-core/src/main/java/com/newrelic/agent/android/analytics/MobileViewEvent.java
+++ agent-core/src/main/java/com/newrelic/agent/android/analytics/MobileViewEvent.java
@@ -8,7 +8,6 @@ package com.newrelic.agent.android.analytics;
 import java.util.Set;
 
 public class MobileViewEvent extends AnalyticsEvent {
-
     public MobileViewEvent(String name, Set<AnalyticsAttribute> attributeSet) {
         super(name, AnalyticsEventCategory.MobileView, AnalyticsEvent.EVENT_TYPE_MOBILE_VIEW, attributeSet);
     }
